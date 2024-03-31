6623257808a29eb6cd8610bacf518f5c7baf7d80
ElenaSmak <shmakova.elena.al@gmail.com>
2024-03-31 21:33:52 +0300

    test_1 test_2 test_3 test_4 test_5 test_6 test_7

---
+++ lesson_1_task_6.py
@@ -0,0 +1,47 @@
+name = 8 
+def print_a():
+ 
+    print(+name, end="")
+name1 =8
+def print_b(): 
+    
+    print(+name1, end="") 
+name2=0
+def print_c():
+    
+    print(+name2, end="") 
+name3=0
+def print_d():
+     
+     print(+name3, end="") 
+name4=5    
+def print_e():
+  
+    print(+name4, end="") 
+name6=5
+def print_f():
+  
+    print(+name6, end="") 
+name7=5
+def print_g():
+    
+    print(+name7, end="") 
+name8=3
+def print_h():
+    
+    print(+name8, end="") 
+name9=5
+def print_i():
+    i=5
+    print(+name9, end="") 
+name10=3
+def print_j():
+    
+    print(+name10, end="") 
+name11=5
+def print_k():
+    
+    print(+name11, end="") 
+  
+
+print_a(),print_b(),print_c(),print_d(),print_e(),print_f(),print_g(),print_h(),print_i(),print_j(),print_k()
